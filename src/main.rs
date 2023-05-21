d1058ec467d8e1803fdfe21e5bbf86f5694e23c9
John Westhoff <jwesthof@nd.edu>
2023-05-21 05:47:15 -0400

    Added chat sample to build around

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,99 @@
-mod server;
-mod player;
-mod environment;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
-fn main() {
-    println!("Hello, world!");
-    let mut s = server::Server::new();
-    s.env_queue("hi".into(), 1);
-    s.login();
-    s.player_walk(0, 3, 3);
-    s.tick();
+use tokio::sync::broadcast;
+use tokio::sync::mpsc;
+use tokio::task;
+use warp::ws::{Message, WebSocket};
+use warp::Filter;
+use futures::{FutureExt, StreamExt, SinkExt};
+
+type PeerMap = Arc<Mutex<HashMap<u64, broadcast::Sender<String>>>>;
+
+#[tokio::main]
+async fn main() {
+    // Initialize the peer map
+    let peer_map: PeerMap = Arc::new(Mutex::new(HashMap::new()));
+
+    // Create a broadcast channel for chat messages
+    let (tx, mut _rx) = broadcast::channel::<String>(100);
+
+    // Spawn a task to listen for incoming messages and broadcast them
+    let peer_clone = peer_map.clone();
+    task::spawn(async move {
+        while let Ok(msg) = _rx.recv().await {
+            // Broadcast the message to all connected peers
+            for (_, sender) in peer_clone.lock().unwrap().iter() {
+                let _ = sender.send(msg.clone());
+            }
+        }
+    });
+
+    // WebSocket route handler
+    let ws_route = warp::path("chat")
+        .and(warp::ws())
+        .and(with_peer_map(peer_map.clone()))
+        .and(with_broadcast(tx.clone()))
+        .map(|ws: warp::ws::Ws, peer_map, tx| {
+            ws.on_upgrade(move |socket| user_connected(socket, peer_map, tx))
+        });
+
+    // Start the server
+    warp::serve(ws_route).run(([127, 0, 0, 1], 3030)).await;
+}
+
+async fn user_connected(ws: WebSocket, peer_map: PeerMap, gtx: broadcast::Sender<String>) {
+    // Create a new channel for the connected user
+    let (tx, mut _rx) = broadcast::channel::<String>(100);
+
+    // Generate a unique ID for the user
+    let user_id = rand::random::<u64>();
+
+    // Save the sender in the peer map
+    peer_map.lock().unwrap().insert(user_id, tx.clone());
+
+    // Forward incoming messages from the user to the broadcast channel
+    let (mut user_ws_tx, mut user_ws_rx) = ws.split();
+    let user_id_clone = user_id;
+    println!("USER CONNECTED!");
+    tokio::task::spawn(async move {
+        while let Some(result) = user_ws_rx.next().await {
+            let message = match result {
+                Ok(msg) => msg,
+                Err(e) => {
+                    eprintln!("WebSocket error: {}", e);
+                    break;
+                }
+            };
+
+            let message = if let Ok(text) = message.to_str() {
+                text.to_owned()
+            } else {
+                continue;
+            };
+
+            println!("GOT MESSAGE {}!", message);
+            let _ = gtx.send(message);
+        }
+
+        // Remove the user from the peer map when the connection is closed
+        peer_map.lock().unwrap().remove(&user_id_clone);
+    });
+
+    // Forward broadcast messages to the connected user
+    while let Ok(msg) = _rx.recv().await {
+        let _ = user_ws_tx.send(Message::text(msg)).await;
+    }
+}
+
+fn with_peer_map(
+    peer_map: PeerMap,
+) -> impl Filter<Extract = (PeerMap,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || peer_map.clone())
+}
+
+fn with_broadcast(
+    gtx: broadcast::Sender<String>,
+) -> impl Filter<Extract = (broadcast::Sender<String>,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || gtx.clone())
 }
